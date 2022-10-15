21e8c8cb8c11b92e884e117828375315027f2d85
c91ihkY <114284654+c91ihkY@users.noreply.github.com>
2022-10-15 12:27:32 +0300

    bebra

---
+++ main.py
@@ -0,0 +1,129 @@
+import os
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import * 
+from PyQt5.QtGui import QPixmap 
+from PIL import Image
+
+
+app = QApplication([])
+win = QWidget()
+win.resize(700,600)
+win.setWindowTitle('Easy Editor bro')
+lb_image = QLabel('Картинка')
+btn_dir = QPushButton('Папка')
+lw_files = QListWidget()
+
+btn1 = QPushButton('Лево')
+btn2 = QPushButton('Право')
+btn3 = QPushButton('Зеркало')
+btn4 = QPushButton('Резкость')
+btn5 = QPushButton('Ч/Б')
+
+row = QHBoxLayout()
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
+col1.addWidget(btn_dir)
+col1.addWidget(lw_files)
+col2.addWidget(lb_image, 95)
+row_tools = QHBoxLayout()
+row_tools.addWidget(btn1)
+row_tools.addWidget(btn2)
+row_tools.addWidget(btn3)
+row_tools.addWidget(btn4)
+row_tools.addWidget(btn5)
+col2.addLayout(row_tools)
+
+row.addLayout(col1, 25)
+row.addLayout(col2, 80)
+win.setLayout(row)
+
+win.show()
+
+workdir = ''
+
+def filter(files, extensions):
+    result = []
+    for filename in files:
+        for ext in extensions:
+            if filename.endswith(ext):
+                result.append(filename)
+    return result
+
+def chooseWorkdir():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+
+def showFilenamesList():
+    extensions = ['.jpg','.jpeg','.png','.gif','.bmp']
+    chooseWorkdir()
+    filenames = filter(os.listdir(workdir), extensions)
+    lw_files.clear()
+    for filename in filenames:
+        lw_files.addItem(filename)
+
+
+btn_dir.clicked.connect(showFilenamesList)
+
+class ImageProcessor():
+    def __init__(self):
+        self.image = None
+        self.dir = None
+        self.filename = None
+        self.save_dir = "Modified/"
+
+    def loadImage(self, dir, filename):
+        self.dir = dir
+        self.filename = filename
+        image_path = os.path.join(dir, filename)
+        self.image = Image.open(image_path)
+
+    def do_bw(self):
+        self.image = self.image.convert("L")
+        self.saveImage()
+        image_path = os.path.join(self.dir, self.save_dir, self.filename)
+        self.showImage(image_path)
+
+
+    def saveImage(self):
+        path = os.path.join(self.dir, self.save_dir)
+        if not (os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+        image_path = os.path.join(path, self.filename)
+        self.image.save(image_path)
+
+    def showImage(self, path):
+        lb_image.hide()
+        pixmapimage = QPixmap(path)
+        w, h = lb_image.width(), lb_image.height()
+        pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+        lb_image.setPixmap(pixmapimage)
+        lb_image.show()
+
+workimage = ImageProcessor()
+
+def showChosenImage():
+    if lw_files.currentRow() >= 0:
+        filename = lw_files.currentItem().text()
+        workimage.loadImage(workdir, filename)
+        image_path = os.path.join(workimage.dir, workimage.filename)
+        workimage.showImage(image_path)
+    
+lw_files.currentRowChanged.connect(showChosenImage)
+btn5.clicked.connect(workimage.do_bw)
+
+
+app.exec()
+
+
+
+
+
+
+       
+
+
+    
+
+
+
+
