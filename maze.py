77a7ce32d2383e217a021d4b91407b4cdccc32ed
c91ihkY <114284654+c91ihkY@users.noreply.github.com>
2022-10-22 10:38:22 +0300

    add a new class

--- maze.py
+++ maze.py
@@ -1,8 +1,12 @@
 from pygame import *
 
-window = display.set_mode((700, 500))
+win_width = 700
+win_height = 500
+
+window = display.set_mode((win_width, win_height))
 display.set_caption('Maze')
-background = transform.scale(image.load('background.jpg'), (700,500))
+background = transform.scale(image.load('background.jpg'), (win_width,win_height))
+
 
 x1 = 100
 y1 = 300
@@ -22,13 +26,34 @@ mixer.init()
 mixer.music.load('jungles.ogg')
 mixer.music.play()
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed 
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+hero = GameSprite('hero.png', 5, win_height - 80, 4)
+cyborg = GameSprite('cyborg.png', win_width - 80, 280, 2)
+final = GameSprite ('treasure.png', win_width - 120, win_height - 80, 0)
+
+        
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     window.blit(background,(0, 0))
-    window.blit(cyborg, (x1, y1))
-    window.blit(hero, (x2, y2))
+
+    hero.reset()
+    cyborg.reset()
+    final.reset()
+
     display.update()
     clock.tick(FPS)
 
